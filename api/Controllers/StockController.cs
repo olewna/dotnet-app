253b821d128c7db7d718b11f0d0da71872a2cda4
olewna <dajcie.suba2002@gmail.com>
2025-06-24 15:08:23 +0200

    create method with mapper and dtos

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Stock;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -20,7 +22,7 @@ namespace api.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var stocks = _context.Stocks.ToList(); // ważne żeby dawać ToList 
+            var stocks = _context.Stocks.ToList().Select(stock => stock.ToStockDto()); // ważne żeby dawać ToList 
             return Ok(stocks);
         }
 
@@ -33,7 +35,16 @@ namespace api.Controllers
             {
                 return NotFound();
             }
-            return Ok(stock);
+            return Ok(stock.ToStockDto());
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+        {
+            var stockModel = stockDto.ToStockFromCreateDto();
+            _context.Stocks.Add(stockModel);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
         }
     }
 }
